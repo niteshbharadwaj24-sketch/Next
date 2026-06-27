7c6ddcc3763c8dd57dcdedf32a1ca0edcfff3e18
niteshbharadwaj24-sketch <niteshbharadwaj24@gmail.com>
2026-06-27 22:13:52 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+use nitesh
+
+CREATE TABLE employee(
+ID INT primary key,
+name varchar(50),
+Age int not null,
+salary int default 25000
+);
+
+drop table employee
+
+select * from employee
+
+
+show  databases;
+
+exec databases
+
+create database  IF NOT Exists nitesh
+
+insert into employee values(1, 'litesh', 29, 1500000);
+insert into employee(id,name,age) values(2,'sp',91);
+insert into student values(3,'PK',29);
+
+select * from employee
